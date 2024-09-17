d5c6d04b4a9d8c609156b41f247de99364d7a98d
obh3705 <72618459+obh3705@users.noreply.github.com>
2024-09-17 13:42:40 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.chat_message("ai").write("hi")
+st.chat_message("human").write("hi")
+st.write("asd")
+
+with st.sidebar:
+    "1. asd"
+    "2. sdf"
